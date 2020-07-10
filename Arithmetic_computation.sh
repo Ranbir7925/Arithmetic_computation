d091ef3978243ad42c02a5c615d83fa61f353d80
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-10 19:51:58 +0530

    Sorted the result in decending order

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -21,3 +21,18 @@ do
 	resultArr[(count++)]=$result
 done
 echo Original Array: ${resultArr[@]}
+
+length=${#resultArr[@]}
+for((i=0;i<$length-1;i++))
+do
+	for((j=0;j<$length-1;j++))
+	do
+		if [ ${resultArr[j]%.*} -lt ${resultArr[j+1]%.*} ]
+		then
+			temp=${resultArr[j]}
+			resultArr[j]=${resultArr[j+1]}
+			resultArr[j+1]=$temp
+		fi
+	done
+done
+echo Descending Order: ${resultArr[@]}
