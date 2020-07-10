0096ceadd164c77dde5afa08aeeb7eb1b4d57762
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-10 19:32:55 +0530

    Stored the result in dictionary

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+declare -A results
 read -p "Enter First Number=" num_1
 read -p "Enter Second Number=" num_2
 read -p "Enter Third Number=" num_3
@@ -8,3 +9,8 @@ result_1=$(($num_1+$num_2*$num_3))
 result_2=$(($num_1*$num_2+$num_3))
 result_3=`echo "scal=2; $num_3*$num_2/$num_3" | bc`
 result_4=$(($num_1%$num_2+$num_3))
+
+results[result_1]=$result_1
+results[result_2]=$result_2
+results[result_3]=$result_3
+results[result_4]=$result_4
